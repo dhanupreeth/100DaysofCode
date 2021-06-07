4ecab8b25dac0d262b43a5ecbaa723d6768096ab
Dhanesh Kumar <dhanupreeth@gmail.com>
2021-06-08 00:04:59 +0530

    Day 1 python code

---
+++ Day-1.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python
+
+print("Hi python")
+print('adding signs "+" in python print function')
+print("Concatenate Examples")
+print("python concatenate" + " " + "with space function") 
+print("python concatenate " + "with space function")
+print("python concatenate" + " with space function")
+print("python concatenate" + "with space function")
+print("python concatenate " + " with space function")
+print('E.g print("print in" + "print function")')
+print("Print in next line\n this is next line message")
+
+input("Which language you are learning? ")
+print("Hi, you are learning  " + input("Which language you are learning now? ") + " " + "ah")
+print( len( input("Python String Count ") ) )
