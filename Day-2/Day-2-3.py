0ec1cf6282d84384a910e959693f84314400b196
Dhanesh Kumar <dhanupreeth@gmail.com>
2021-11-08 08:27:01 +0530

    int function updated

--- Day-2/Day-2-3.py
+++ Day-2/Day-2-3.py
@@ -3,17 +3,16 @@ import datetime
 from calendar import monthrange
 
 year = input("Enter the year: ")
-month_num = input("Enter the month in number Example - 2, the Feburary month will be taken: ")
-month = int(month_num)
+month = int(input("Enter the month in number Example - 2, the Feburary month will be taken: "))
 num_days = monthrange(int(year), month)[1]
 per_day_ineterest = input("Interest Per Day: ")
 
 # Enter the Principle Amount 
-principle_amount = input("Enter the principle amount, Example - 500000 : ")
+principle_amount = int(input("Enter the principle amount, Example - 500000 : "))
 print("Interest will be calculate for", num_days, "Days")
 interest_rate = num_days * int(per_day_ineterest)
 print("Per Lac the interest rate is:", interest_rate)
-total_interest = (int(principle_amount) / 100000)  * int(interest_rate)
+total_interest = (principle_amount / 100000)  * int(interest_rate)
 total_interest_as_init = int(total_interest)
 print(f"Your Payable Interest rate for the {month}th month is {total_interest_as_init}")
 
